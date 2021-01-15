29f6c90e863fe4f3f0255a6e39b8340ee82e6127
Nils Hoffmann <nils.hoffmann@god.de>
2021-01-15 19:16:53 +0100

    Added Day 7

---
+++ src/main/kotlin/dev/nilshoffmann/day7/Bag.kt
@@ -0,0 +1,43 @@
+package dev.nilshoffmann.day7
+
+class Bag(private val color: String, definition: String) {
+    companion object {
+        fun createFromSentences(lines: List<String>): Map<String, Bag> {
+            val colorAndDefinitionGroups = """^(.+) bags contain(.+)$""".toRegex()
+            return lines.map {
+                val (color, definition) = colorAndDefinitionGroups.find(it)!!.destructured
+                color to Bag(color, definition)
+            }.toMap()
+        }
+    }
+
+    private var containableCountByColor: Map<String, Int> = if (definition.contains("no other")) {
+        emptyMap()
+    } else {
+        val singleDefinition = """(\d)(.*)bags?\.?""".toRegex()
+        definition
+            .split(",")
+            .map { it.trim() }
+            .map {
+                val (count, color) = singleDefinition.find(it)!!.destructured
+                color.trim() to Integer.parseInt(count)
+            }.toMap()
+    }
+
+    fun canContain(color: String, allBags: Map<String, Bag>): Boolean {
+        return when {
+            containableCountByColor.isEmpty() -> false
+            containableCountByColor.containsKey(color) -> true
+            else -> containableCountByColor.keys.map { allBags[it] }.any { it!!.canContain(color, allBags) }
+        }
+    }
+
+    fun countContainedBags(allBags: Map<String, Bag>): Int {
+        return containableCountByColor.entries.map { it.value + it.value * allBags[it.key]!!.countContainedBags(allBags) }.sum()
+    }
+
+    override fun toString(): String {
+        return "$color -> [$containableCountByColor]"
+    }
+
+}
---
+++ src/test/kotlin/dev/nilshoffmann/day7/Day7Test.kt
@@ -0,0 +1,30 @@
+package dev.nilshoffmann.day7
+
+import dev.nilshoffmann.load
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day7Test {
+    companion object {
+        const val DAY7_TEST_DATA_FILE = "/inputs/day-7-test.txt"
+        const val DAY7_2_TEST_DATA_FILE = "/inputs/day-7.2-test.txt"
+    }
+
+    @Test
+    fun `Part One Example is correctly calculated`() {
+        val fittingBags = solveDay7PartOne(load(DAY7_TEST_DATA_FILE), "shiny gold")
+        Assertions.assertEquals(4, fittingBags)
+    }
+
+    @Test
+    fun `Part Two Example One is correctly calculated`() {
+        val bagCount = solveDay7PartTwo(load(DAY7_TEST_DATA_FILE), "shiny gold")
+        Assertions.assertEquals(32, bagCount)
+    }
+
+    @Test
+    fun `Part Two Example Two is correctly calculated`() {
+        val bagCount = solveDay7PartTwo(load(DAY7_2_TEST_DATA_FILE), "shiny gold")
+        Assertions.assertEquals(126, bagCount)
+    }
+}
