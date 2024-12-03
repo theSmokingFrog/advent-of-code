3670856fb642bce2a073c93d79b5c5ddccc7788e
Nils Hoffmann <nils.hoffmann@hyand.com>
2024-12-03 14:54:20 +0100

    24-02: Quick Refactor to improve readability

--- src/main/kotlin/dev/nhoffmann/aoc/y2024/day2/Report.kt
+++ src/main/kotlin/dev/nhoffmann/aoc/y2024/day2/Report.kt
@@ -1,12 +1,8 @@
 package dev.nhoffmann.aoc.y2024.day2
 
-class Report(data: String) {
-    companion object {
-        fun create(line: String): Report {
-            return Report(line)
-        }
-    }
+import kotlin.math.abs
 
+class Report(data: String) {
     private val levels: List<Int> = data.split(" ").map { it.toInt() }
 
     fun isSafe(): Boolean {
@@ -17,7 +13,7 @@ class Report(data: String) {
         val levelChanges = levels.windowed(2).map { (left, right) -> left - right }
 
         val allIncreasingOrDecreasing = levelChanges.all { it > 0 } || levelChanges.all { it < 0 }
-        val levelsWithinBoundaries = levelChanges.all { it in 1..3 } || levelChanges.all { it in -3..-1 }
+        val levelsWithinBoundaries = levelChanges.all { abs(it) in 1..3 }
 
         return allIncreasingOrDecreasing && levelsWithinBoundaries;
     }
@@ -34,7 +30,7 @@ class Report(data: String) {
     private fun evaluateDampenedLevels(): Boolean {
         val levelVariants = levels.indices.map { levels.filterIndexed { index, _ -> index != it } }
 
-        return levelVariants.stream().anyMatch { areLevelsSafe(it) }
+        return levelVariants.any { areLevelsSafe(it) }
     }
 
     override fun toString(): String {
